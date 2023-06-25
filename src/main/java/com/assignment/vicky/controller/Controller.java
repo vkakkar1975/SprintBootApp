8bbbbb872e60e6add2afca5f35178ad0f78f923c
Vicky <vkakkar1975@gmail.com>
2023-06-25 14:13:07 -0400

    added controller

---
+++ src/main/java/com/assignment/vicky/controller/Controller.java
@@ -0,0 +1,32 @@
+package com.assignment.vicky.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime; 
+import java.util.Locale; 
+
+@RestController
+public class Controller {
+
+	@GetMapping("/getMessage")
+	public String getMessage() {
+		return "My final assignment";
+	}
+	@GetMapping("/getDate")
+	public String getDate() {
+       DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+   		LocalDateTime now = LocalDateTime.now();  
+   		return dtf.format(now);
+	
+	}
+	@GetMapping("/getLocale")
+	public String getLocale() {
+		Locale locale = Locale.getDefault();
+        String lang = locale.getDisplayLanguage();
+        String country = locale.getDisplayCountry();     
+   		return lang +" " +country;
+	
+	}
+
+}
